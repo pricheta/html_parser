d708f2055fc76892c8827232a132fda3b134be7d
pricheta <pricheta@inbox.ru>
2024-08-24 21:33:26 +0300

    v0.1

--- app.py
+++ app.py
@@ -1,6 +1,16 @@
 from bs4 import BeautifulSoup
+from bs4.element import ResultSet
 
-html_file_path = "files/file.html"
+from models.FileBuilder import BruteForceFileBuilder, IFileBuilder
 
-with open(html_file_path, "r") as my_file:
-    pass
+html_file_path: str = "files/file.html"
+parents_class: str = "cc-fl ng-scope"
+attrs: dict[str, str] = {
+    "class": parents_class,
+}
+
+with open(html_file_path, "r", encoding="utf-8") as html_file:
+    bs = BeautifulSoup(html_file, features="html.parser")
+result_set: ResultSet = bs.find_all(name="div", attrs=attrs)
+file_builder: IFileBuilder = BruteForceFileBuilder(result_set=result_set)
+file_builder.build()
---
+++ models/FileBuilder.py
@@ -0,0 +1,28 @@
+from abc import ABC, abstractmethod
+from datetime import datetime
+
+from bs4.element import ResultSet
+import pandas as pd
+
+
+class IFileBuilder(ABC):
+    def __init__(self, result_set: ResultSet) -> None:
+        self._result_set = result_set
+        ...
+
+    @abstractmethod
+    def build(self) -> None:
+        ...
+
+
+class BruteForceFileBuilder(IFileBuilder):
+
+    def build(self) -> None:
+        result_list: list[list[str]] = []
+        for element in self._result_set:
+            element_info: list[str] = [value for value in element.stripped_strings]
+            result_list.append(element_info)
+
+        result_df: pd.DataFrame = pd.DataFrame(data=result_list)
+        result_file_path:str = "files/Итоговый файл.xlsx"
+        result_df.to_excel(result_file_path)
