db5c829806f6e75208dd2542bc1dab74e2d116de
pricheta <pricheta@inbox.ru>
2024-08-25 20:28:36 +0300

    v1.0.0 Мелкая правка

--- models/Settings.py
+++ models/Settings.py
@@ -1,8 +1,9 @@
 from dataclasses import dataclass
+from datetime import datetime
 
 @dataclass
 class Settings:
     """Класс для хранения настроек по работе проекта"""
     element_classes: str
     html_file_path: str = "files/file.html"
-    result_file_path: str = "files/Итоговый файл.xlsx"
+    result_file_path: str = "files/Итоговый файл от {}.xlsx".format(datetime.now().strftime("%H-%M"))
