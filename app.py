4ddd36ac254104a465ec4a2f95cf6683b0e384c5
pricheta <pricheta@inbox.ru>
2024-08-25 08:51:12 +0300

    Revert "[в разработке] v0.1.2"
    This reverts commit 20b6039bcc2dd5b2af4ff2bc2be0e1222dbee7f9.

--- app.py
+++ app.py
@@ -3,33 +3,23 @@ from tkinter import Tk
 from bs4 import BeautifulSoup
 from bs4.element import ResultSet
 
-from models.Settings import Settings
-from models.GUIBuilder import IGUIBuilder, GUIBuilder
 from models.FileBuilder import BruteForceFileBuilder, IFileBuilder
+from models.GUIBuilder import IGUIBuilder, GUIBuilder
 
+window: Tk = GUIBuilder.build()
+window.mainloop()
 
-if __name__ == "__main__":
+settings.html_file_path: str = "files/file.html"
+parents_class: str = "cc-fl ng-scope"
+attrs: dict[str, str] = {
+    "class": parents_class,
+}
 
-    window: Tk = GUIBuilder.build()
-    window.mainloop()
+with open(settings.html_file_path, "r", encoding="utf-8") as html_file:
+    bs = BeautifulSoup(html_file, features="html.parser")
+result_set: ResultSet = bs.find_all(name="div", attrs=attrs)
+file_builder: IFileBuilder = BruteForceFileBuilder(result_set=result_set)
+file_builder.build()
 
-
-
-
-
-print("fdsfds")
-
-
-
-    # settings.html_file_path: str = "files/file.html"
-    # parents_class: str = "cc-fl ng-scope"
-    # attrs: dict[str, str] = {
-    #     "class": parents_class,
-    # }
-    #
-    # with open(settings.html_file_path, "r", encoding="utf-8") as html_file:
-    #     bs = BeautifulSoup(html_file, features="html.parser")
-    # result_set: ResultSet = bs.find_all(name="div", attrs=attrs)
-    # file_builder: IFileBuilder = BruteForceFileBuilder(result_set=result_set)
-    # file_builder.build()
-
+def start_app():
+    pass
--- models/GUIBuilder.py
+++ models/GUIBuilder.py
@@ -1,10 +1,5 @@
 from abc import ABC, abstractmethod
-from functools import cached_property
-from linecache import cache
-from tkinter import Tk, Button, Entry, Label
-
-from models.Settings import Settings, SettingsBuilder
-
+from tkinter import Tk, Button
 
 class IGUIBuilder(ABC):
 
@@ -16,23 +11,14 @@ class IGUIBuilder(ABC):
 class GUIBuilder(IGUIBuilder):
 
     @staticmethod
+    @abstractmethod
     def build() -> Tk:
-        window: Tk = Tk()
+        window = Tk()
         window.title("Парсинг для Влады")
         window.geometry("300x300")
         window.resizable(False, False)
 
-        label = Label(text="Введи класс(ы) элементов")
-        label.pack()
-
-        classes_entry: Entry = Entry(width=40)
-        classes_entry.pack()
-
-        start_button: Button = Button(text="Запуск")
+        start_button = Button(text="Запуск")
         start_button.pack()
 
         return window
-
-
-def buildSettings(settings: Settings):
-    pass
