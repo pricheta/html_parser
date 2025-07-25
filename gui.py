6e4c5bd7f4fd107afef12dd13cbb4c8691fb8e34
pricheta <pricheta@inbox.ru>
2025-07-25 19:59:39 +0300

    Рефакторинг

--- gui.py
+++ gui.py
@@ -1,4 +1,3 @@
-import os
 from threading import Thread
 
 from tkinter import Tk, Button, Entry, Label
@@ -30,8 +29,6 @@ def get_gui() -> GUI:
     _pack_element(hint_label)
 
     classes_search_row: Entry = Entry(window, justify="center", font=('Calibri', 14), width=25)
-    cached_classes_value = _get_cached_classes_value()
-    classes_search_row.insert(index=0, string=cached_classes_value)
     _pack_element(classes_search_row)
 
     start_button: Button = Button(window, text="Запуск", command=_run, width=20, relief="groove", height=1)
@@ -47,13 +44,6 @@ def get_gui() -> GUI:
         start_button=start_button,
         exit_button=exit_button,
     )
-
-def _get_cached_classes_value() -> str:
-    cache_file_path = "cache.txt"
-    if os.path.isfile(cache_file_path):
-        with open(cache_file_path, "r", encoding="utf-8") as temp_file:
-            return temp_file.read()
-    return ""
 
 def _pack_element(element: GUI_ELEMENT) -> None:
     element.pack(fill="y", ipadx=10, ipady=5, padx=10, pady=5)
