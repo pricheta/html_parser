f5f68887db2ac3651a026baa50c30dd24868ded4
pricheta <pricheta@inbox.ru>
2025-07-25 18:48:19 +0300

    Рефакторинг

--- models/GUI.py
+++ models/GUI.py
@@ -1,84 +1,58 @@
-import sys
 import os
 
-from abc import ABC, abstractmethod
-from tkinter import Tk, Button, Entry, Label, Frame
+from tkinter import Tk, Button, Entry, Label
+from pydantic import BaseModel, ConfigDict
 
 
-class GUI:
-    """Класс для хранения информации по пользовательскому интерфейсу
+GUI_ELEMENT = Tk | Label | Entry | Button
 
-    Attributes
-    ----------
+
+class GUI(BaseModel):
+    model_config = ConfigDict(arbitrary_types_allowed=True)
+
     window: Tk
-        Окно приложения
     hint_label: Label
-        Лейбл с подсказками. Помогает пользователю понять, что от него требуется
-    finish_label: Label
-        Финишный лейбл. Подсказывает результат работы
-    element_classes: Entry
-        Поле ввода классов, с помощью которого будут найдены нужные элементы
+    classes_search_row: Entry
     start_button: Button
-        Кнопка запуска проекта
     exit_button: Button
-        Кнопка выхода из проекта
-    """
-    def __init__(self, window:Tk, hint_label: Label, finish_label: Label,  element_classes: Entry, start_button: Button, exit_button: Button) -> None:
-        self.window = window
-        self.hint_label = hint_label
-        self.finish_label = finish_label
-        self.element_classes = element_classes
-        self.start_button = start_button
-        self.exit_button = exit_button
 
 
-class IGUIBuilder(ABC):
-    """ Интерфейс для билдера пользовательского интерфейса"""
 
-    @staticmethod
-    @abstractmethod
-    def build() -> GUI:
-        ...
+def get_gui() -> GUI:
+    window: Tk = Tk()
+    window.title("HTML-парсер")
+    window.geometry("300x300")
+    window.resizable(False, False)
 
 
-class GUIBuilder(IGUIBuilder):
-    """ Билдер пользовательского интерфейса"""
+    hint_label = Label(window, text="Введи класс(ы) нужных элементов")
+    pack_element(hint_label)
 
-    @staticmethod
-    def build() -> GUI:
-        window: Tk = Tk()
-        window.title("Парсинг для Влады")
-        window.geometry("300x300")
-        window.resizable(False, False)
+    classes_search_row: Entry = Entry(window, justify="center", font=('Calibri', 14), width=25)
+    cached_classes_value = get_cached_classes_value()
+    classes_search_row.insert(index=0, string=cached_classes_value)
+    pack_element(classes_search_row)
 
-        window.grid_rowconfigure(0, weight=1)
-        window.grid_columnconfigure(0, weight=1)
+    start_button: Button = Button(window, text="Запуск", command=lambda: print('hello'), width=20, relief="groove", height=1)
+    pack_element(start_button)
 
-        container = Frame(window)
-        container.grid(row=0, column=0)
+    exit_button: Button = Button(window, text="Закрыть", command=window.quit, width=20, relief="groove", height=1)
+    pack_element(exit_button)
 
-        hint_label = Label(container, text="Введи класс(ы) нужных элементов")
-        hint_label.grid(row=0, column=0)
+    return GUI(
+        window=window,
+        hint_label=hint_label,
+        classes_search_row=classes_search_row,
+        start_button=start_button,
+        exit_button=exit_button,
+    )
 
-        finish_label = Label(container, text="")
+def get_cached_classes_value() -> str:
+    cache_file_path = "cache.txt"
+    if os.path.isfile(cache_file_path):
+        with open(cache_file_path, "r", encoding="utf-8") as temp_file:
+            return temp_file.read()
+    return ""
 
-        element_classes: Entry = Entry(container, justify="center", font=('Calibri', 14), width=25)
-        if os.path.isfile("files/project/temp.txt"):
-            with open("files/project/temp.txt", "r", encoding="utf-8") as temp_file:
-                element_classes.insert(index=0, string=temp_file.read())
-        element_classes.grid(row=1, column=0)
-
-        start_button: Button = Button(container, text="Запуск", command=window.quit, width=20, relief="groove", height=1)
-        start_button.grid(row=2, column=0, pady=(10, 5))
-
-        exit_button: Button = Button(container, text="Закрыть", command=sys.exit, width=20, relief="groove", height=1)
-        exit_button.grid(row=3, column=0, pady=(5, 10))
-
-        return GUI(
-            window=window,
-            hint_label=hint_label,
-            finish_label=finish_label,
-            element_classes=element_classes,
-            start_button=start_button,
-            exit_button=exit_button,
-        )
+def pack_element(element: GUI_ELEMENT) -> None:
+    element.pack(fill="y", ipadx=10, ipady=5, padx=10, pady=5)
