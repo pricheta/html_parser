14bde5c660a08b8d6ffdd31997c2462fb0e4c34a
pricheta <pricheta@inbox.ru>
2025-07-31 20:34:50 +0300

    Рефакторинг

--- logger/logger.py
+++ logger/logger.py
@@ -3,7 +3,7 @@ import logging
 
 logging.basicConfig(
     level=logging.INFO,
-    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    format='%(asctime)s - %(message)s',
     handlers=[
         logging.StreamHandler(),
         logging.FileHandler('logs.log', encoding='utf-8'),
