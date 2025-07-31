a8e65d069c2e24e72f071c2d1048a53c6b4f11cf
pricheta <pricheta@inbox.ru>
2025-07-31 21:55:34 +0300

    Рефакторинг

--- app.py
+++ app.py
@@ -27,7 +27,7 @@ def log_starting_session(session_id: UUID) -> None:
 
 def log_ending_session(session_id: UUID) -> None:
     now = datetime.now().strftime('%d.%m.%Y %H:%M')
-    logger.info(f'---------------------- Ending session {session_id} at {now} -------------------------')
+    logger.info(f'---------------------- Ending session {session_id} at {now} -------------------------\n\n\n')
 
 
 if __name__ == "__main__":
