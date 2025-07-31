3fcaafd0389b1c5a288bdd88be79178daaaa8228
pricheta <pricheta@inbox.ru>
2025-07-31 20:05:40 +0300

    Рефакторинг

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
 from parsers.chrome_parser import ChromeParser
 from parsers.file_parser import FileParser
 from questioner import questioner
+from user_answers import AppMode
 
 if __name__ == "__main__":
     user_answers = questioner.question_user()
+    print(user_answers)
 
-    if user_answers.use_url:
+    if user_answers.app_mode == AppMode.FROM_URL:
         parser = ChromeParser(user_answers)
     else:
         parser = FileParser(user_answers)
