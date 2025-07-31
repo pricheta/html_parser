6b8a728e2db162245ce9dc713689658ee4db2580
pricheta <pricheta@inbox.ru>
2025-07-31 21:43:52 +0300

    Рефакторинг

--- logger/logger.py
+++ logger/logger.py
@@ -1,6 +1,7 @@
 import logging
 from typing import Callable
 
+
 logging.basicConfig(
     level=logging.INFO,
     format='%(message)s',
@@ -18,4 +19,3 @@ def log_calling(func: Callable):
         logging.info(f"Calling {func.__qualname__} with {args=}, {kwargs=}")
         return func(*args, **kwargs)
     return wrapper
-
