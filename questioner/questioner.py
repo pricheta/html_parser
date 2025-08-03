d97cbbf9045e72e58e0360f4821201b5e99e3bf3
pricheta <pricheta@inbox.ru>
2025-08-03 18:05:04 +0300

    Рефакторинг

--- questioner/questioner.py
+++ questioner/questioner.py
@@ -36,17 +36,17 @@ class Questioner:
         self._ask_user(master_slave_mode=questionary.select("Режим переходов на второстепенные страницы", [mode for mode in MasterSlaveMode], instruction=' '))
         self._ask_user(scroll_required=questionary.confirm("Нужно ли будет скроллить вниз основную страницу?"))
         self._ask_user(url=questionary.text('Ссылка на основную страницу для парсинга:', validate=bool))
-        self._ask_user(master_page_parsed_selector=questionary.text("Введи классы элементов для парсинга на основной странице:", validate=bool))
+        self._ask_user(master_page_parsed_selector=questionary.text("Введи селектор элементов для парсинга на основной странице:", validate=bool))
 
         if self.user_answers.master_slave_mode == MasterSlaveMode.CLICK_INNER_TAG:
-            self._ask_user(clicked_selector=questionary.text("Введи классы элементов, на которые нужно нажать для перехода на вторичные страницы:", validate=bool))
+            self._ask_user(clicked_selector=questionary.text("Введи селектор элементов, на которые нужно нажать для перехода на вторичные страницы:", validate=bool))
 
         if self.user_answers.master_slave_mode in (MasterSlaveMode.CLICK_MASTER_TAG, MasterSlaveMode.CLICK_INNER_TAG):
-            self._ask_user(slave_page_parsed_selector=questionary.text("Введи классы элементов для парсинга на вторичных страницах:", validate=bool))
+            self._ask_user(slave_page_parsed_selector=questionary.text("Введи селектор элементов для парсинга на вторичных страницах:", validate=bool))
 
         self._ask_user(delay=questionary.text("Введи задержку между действиями:", validate=bool))
 
     def _ask_about_file_mode(self):
-        self._ask_user(master_page_parsed_selector=questionary.text("Введи классы элементов для парсинга на странице:", validate=bool))
+        self._ask_user(master_page_parsed_selector=questionary.text("Введи селектор элементов для парсинга на странице:", validate=bool))
 
 questioner = Questioner()
