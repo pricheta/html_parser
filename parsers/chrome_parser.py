7769d56df5fb427ce603eefe648f02111b33bcc5
pricheta <pricheta@inbox.ru>
2025-08-03 07:32:03 +0300

    Скролл

--- parsers/chrome_parser.py
+++ parsers/chrome_parser.py
@@ -5,6 +5,7 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
 from webdriver_manager.chrome import ChromeDriverManager
 
 from logger.logger import log_calling
@@ -12,8 +13,9 @@ from parsers.parser_interface import Parser
 
 
 class Chrome:
-    def __init__(self, delay: int):
+    def __init__(self, delay: int, scroll_required: bool):
         self.delay = delay
+        self.scroll_required = scroll_required
 
     def __enter__(self):
         options = Options()
@@ -34,12 +36,15 @@ class Chrome:
         clicked_classes: str | None,
         slave_page_parsed_classes: str | None,
     ) -> list[str]:
-        self.driver.get(url)
-        sleep(self.delay)
-
         html_files = []
         main_window = self.driver.current_window_handle
-        current_url = self.driver.current_url
+
+        self.driver.get(url)
+        self._wait_till_page_loaded()
+
+        if self.scroll_required:
+            self._scroll_to_bottom_with_wait()
+
         master_page_blocks = self.driver.find_elements(By.CLASS_NAME, master_page_parsed_classes)
 
         for i in range(len(master_page_blocks)):
@@ -47,14 +52,13 @@ class Chrome:
             html_content = master_page_blocks[i].get_attribute('outerHTML')
 
             if clicked_classes:
-                self.driver.execute_script(f"window.open('{current_url}', '_blank');")
-                new_window = [window for window in self.driver.window_handles if window != main_window][0]
+                new_window = self._duplicate_tab_full()
                 self.driver.switch_to.window(new_window)
 
                 master_page_blocks = self.driver.find_elements(By.CLASS_NAME, master_page_parsed_classes)
                 clicked_block = master_page_blocks[i].find_element(By.CLASS_NAME, clicked_classes)
                 self.driver.execute_script("arguments[0].click();", clicked_block)
-                sleep(self.delay)
+                self._wait_till_page_loaded()
 
                 slave_block = self.driver.find_element(By.CLASS_NAME, slave_page_parsed_classes)
                 html_content += slave_block.get_attribute('outerHTML')
@@ -66,12 +70,66 @@ class Chrome:
 
         return html_files
 
+    def _wait_till_page_loaded(self):
+        WebDriverWait(self.driver, self.delay).until(
+            lambda d: d.execute_script("return document.readyState") == "complete"
+        )
+
+
+    def _scroll_to_bottom_with_wait(self):
+        last_height = self.driver.execute_script("return document.body.scrollHeight")
+        while True:
+            self.driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+            try:
+                WebDriverWait(self.driver, self.delay).until(
+                    lambda d: d.execute_script("return document.body.scrollHeight") > last_height
+                )
+                last_height = self.driver.execute_script("return document.body.scrollHeight")
+            except:
+                break
+
+    def _duplicate_tab_full(self):
+        state = {
+            'url': self.driver.current_url,
+            'scroll': self.driver.execute_script("return [window.pageXOffset, window.pageYOffset];"),
+            'html': self.driver.execute_script("return document.documentElement.outerHTML;"),
+            'cookies': self.driver.get_cookies(),
+            'local_storage': self.driver.execute_script("return JSON.stringify(localStorage);"),
+            'session_storage': self.driver.execute_script("return JSON.stringify(sessionStorage);")
+        }
+
+        self.driver.switch_to.new_window('tab')
+
+        self.driver.execute_script(f"""
+            document.open();
+            document.write(`{state['html']}`);
+            document.close();
+            window.scrollTo({state['scroll'][0]}, {state['scroll'][1]});
+            history.replaceState(null, null, `{state['url']}`);
+
+            const localStorageData = {state['local_storage']};
+            const sessionStorageData = {state['session_storage']};
+
+            for (const key in localStorageData) {{
+                localStorage.setItem(key, localStorageData[key]);
+            }}
+
+            for (const key in sessionStorageData) {{
+                sessionStorage.setItem(key, sessionStorageData[key]);
+            }}
+        """)
+
+        for cookie in state['cookies']:
+            self.driver.add_cookie(cookie)
+
+        return self.driver.current_window_handle
+
 
 
 class ChromeParser(Parser):
     @log_calling
     def parse(self) -> list[list[str]]:
-        chrome = Chrome(delay=2)
+        chrome = Chrome(delay=2, scroll_required=self.user_answers.scroll_required)
         parse_result = []
         with chrome:
             html_files=chrome.collect_html_content(
